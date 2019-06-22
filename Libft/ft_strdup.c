ce6188874d91d3486665b889163c5d4145a12bf3
Arnolf Ernie <aernie@mi-m6.21-school.ru>
2019-06-22 19:07:12 +0300

    Libft

--- Libft/ft_strdup.c
+++ Libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: aernie <aernie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 11:35:15 by aernie            #+#    #+#             */
-/*   Updated: 2019/06/22 16:42:11 by aernie           ###   ########.fr       */
+/*   Updated: 2019/06/22 19:05:29 by aernie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ char	*ft_strdup(const char *s1)
 
 	i = 0;
 	j = 0;
-	while (s1[j] != '\0')
+	while (s1[j])
 		j++;
-	if (!(s2 == (char*)malloc(sizeof(*s2) * (j + 1))))
+	if (!(s2 = (char*)malloc(sizeof(*s2) * (j + 1))))
 		return (NULL);
 	while (s1[i])
 	{
