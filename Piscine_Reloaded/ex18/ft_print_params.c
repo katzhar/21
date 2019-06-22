6998dbabd73b725ec199c5f8dc13d071a0a45ca0
Arnolf Ernie <aernie@mi-m6.21-school.ru>
2019-06-22 17:36:50 +0300

    Piscine_Reloaded

---
+++ Piscine_Reloaded/ex18/ft_print_params.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aernie <aernie@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/02 17:46:22 by aernie            #+#    #+#             */
+/*   Updated: 2019/06/22 16:53:21 by aernie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+int		main(int argc, char const **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		i++;
+		ft_putchar('\n');
+	}
+	return (0);
+}
---
+++ Piscine_Reloaded/ex27/srcs/open.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   open.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aernie <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/07 21:38:17 by aernie            #+#    #+#             */
+/*   Updated: 2019/04/07 23:46:56 by aernie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#define SIZE 1024
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	int		n;
+	char	str[SIZE];
+
+	if (argc == 1)
+	{
+		write(1, "File name missing.\n", 19);
+		return (0);
+	}
+	if (argc > 2)
+	{
+		write(1, "Too many arguments.\n", 20);
+		return (0);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd > 0)
+	{
+		n = SIZE;
+		while (n == SIZE)
+		{
+			n = read(fd, str, SIZE);
+			write(1, &str[0], n);
+		}
+	}
+	return (0);
+}
